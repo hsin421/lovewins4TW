954b0d92a97a6eaa264fc5230d44b01e22e28852
Hsin <shinsonh@gmail.com>
2016-11-30 00:44:07 -0500

    change port

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var app = express();
 var router = express.Router();
 
 //Set our port to either a predetermined port number if you have set it up, or 3001
-var port = process.env.API_PORT || 3001;
+var port = process.env.PORT || 3001;
 
 
 //Configure the API to use bodyParser and look for JSON data in the request body
